e6bfa4dfc96fe714b2794cc817394e8155fd099e
imtpot <imtpot@mayflower.work>
2024-07-17 16:11:27 +0400

    first commit

---
+++ __main__.py
@@ -0,0 +1,57 @@
+from pulumi import ComponentResource, ResourceOptions, export
+from pulumi_command import local
+from pulumi_kubernetes import Provider
+from pulumi_kubernetes.core.v1 import Secret
+from pulumi_kubernetes.helm.v3 import Release, RepositoryOptsArgs
+
+class KindCluster(ComponentResource):
+    def __init__(self, name: str, opts=None):
+        super().__init__('custom:KindCluster', name, None, opts)
+        
+        self.cluster = local.Command(
+            f'{name}-create',
+            create=f'kind create cluster --name {name}',
+            delete=f'kind delete cluster --name {name}',
+            opts=ResourceOptions(parent=self)
+        )
+        
+        self.kubeconfig = local.Command(
+            f'{name}-kubeconfig',
+            create=f'kind get kubeconfig --name {name}',
+            opts=ResourceOptions(parent=self, depends_on=[self.cluster])
+        )
+        
+        self.provider = Provider(
+            f'{name}-provider',
+            kubeconfig=self.kubeconfig.stdout,
+            opts=ResourceOptions(parent=self, depends_on=[self.kubeconfig])
+        )
+
+class ArgoCD(ComponentResource):
+    def __init__(self, name: str, cluster: KindCluster, opts=None):
+        super().__init__('custom:ArgoCD', name, None, opts)
+        
+        self.argocd = Release(
+            name,
+            chart='argo-cd',
+            version='7.3.7',
+            repository_opts=RepositoryOptsArgs(repo='https://argoproj.github.io/argo-helm'),
+            namespace='argocd',
+            create_namespace=True,
+            opts=ResourceOptions(provider=cluster.provider, parent=self)
+        )
+
+        self.secret = Secret.get(
+            f'{name}-secret',
+            'argocd/argocd-initial-admin-secret',
+            opts=ResourceOptions(provider=cluster.provider, parent=self, depends_on=[self.argocd])
+        )
+
+cluster_1 = KindCluster('cluster-1')
+cluster_2 = KindCluster('cluster-2')
+
+argocd_1 = ArgoCD('argocd-1', cluster_1)
+argocd_2 = ArgoCD('argocd-2', cluster_2)
+
+export('argocd-1', argocd_1.secret.data['password'])
+export('argocd-2', argocd_2.secret.data['password'])
