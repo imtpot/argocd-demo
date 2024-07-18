1402bd7ec7666f80c41c290e32feb6cc7cf81ce3
imtpot <imtpot@mayflower.work>
2024-07-18 10:06:25 +0400

    chore: Update ArgoCD infrastructure to include app-config.yaml for chart version 6.6.3

--- __main__.py
+++ __main__.py
@@ -1,55 +1,62 @@
-from pulumi import ComponentResource, ResourceOptions, export
+from pulumi import ComponentResource, ResourceOptions, Output, export
 from pulumi_command import local
 from pulumi_kubernetes import Provider
 from pulumi_kubernetes.core.v1 import Secret
 from pulumi_kubernetes.helm.v3 import Release, RepositoryOptsArgs
+from base64 import b64decode
 
 class KindCluster(ComponentResource):
     def __init__(self, name: str, opts=None):
-        super().__init__('custom:KindCluster', name, None, opts)
+        super().__init__("custom:KindCluster", name, None, opts)
         
         self.cluster = local.Command(
-            f'{name}-create',
-            create=f'kind create cluster --name {name}',
-            delete=f'kind delete cluster --name {name}',
+            f"{name}-create",
+            create=f"kind create cluster --name {name}",
+            delete=f"kind delete cluster --name {name}",
             opts=ResourceOptions(parent=self)
         )
         
         self.kubeconfig = local.Command(
-            f'{name}-kubeconfig',
-            create=f'kind get kubeconfig --name {name}',
+            f"{name}-kubeconfig",
+            create=f"kind get kubeconfig --name {name}",
             opts=ResourceOptions(parent=self, depends_on=[self.cluster])
         )
         
         self.provider = Provider(
-            f'{name}-provider',
+            f"{name}-provider",
             kubeconfig=self.kubeconfig.stdout,
             opts=ResourceOptions(parent=self, depends_on=[self.kubeconfig])
         )
 
 class ArgoCD(ComponentResource):
     def __init__(self, name: str, cluster: KindCluster, opts=None):
-        super().__init__('custom:ArgoCD', name, None, opts)
+        super().__init__("custom:ArgoCD", name, None, opts)
         
         self.argocd = Release(
             name,
             name="argocd",
-            chart='argo-cd',
-            version='7.3.7',
-            repository_opts=RepositoryOptsArgs(repo='https://argoproj.github.io/argo-helm'),
-            namespace='argocd',
+            chart="argo-cd",
+            version="7.3.7",
+            repository_opts=RepositoryOptsArgs(repo="https://argoproj.github.io/argo-helm"),
+            namespace="argocd",
             create_namespace=True,
-            opts=ResourceOptions(provider=cluster.provider, parent=self)
+            opts=ResourceOptions(provider=cluster.provider, parent=self, depends_on=[cluster.cluster])
         )
 
         self.secret = Secret.get(
-            f'{name}-secret',
-            'argocd/argocd-initial-admin-secret',
+            f"{name}-secret",
+            "argocd/argocd-initial-admin-secret",
             opts=ResourceOptions(provider=cluster.provider, parent=self, depends_on=[self.argocd])
         )
 
-cluster_1 = KindCluster('cluster-1')
-cluster_2 = KindCluster('cluster-2', opts=ResourceOptions(depends_on=[cluster_1]))
+def decode_password(password: Output[str]) -> Output[str]:
+    return password.apply(lambda p: b64decode(p).decode('utf-8'))
 
-argocd_1 = ArgoCD('argocd-1', cluster_1)
-argocd_2 = ArgoCD('argocd-2', cluster_2)
+cluster_1 = KindCluster("cluster-1")
+cluster_2 = KindCluster("cluster-2", opts=ResourceOptions(depends_on=[cluster_1]))
+
+argocd_1 = ArgoCD("argocd-1", cluster_1)
+argocd_2 = ArgoCD("argocd-2", cluster_2)
+
+export("argo-1-secret", decode_password(argocd_1.secret.data["password"]))
+export("argo-2-secret", decode_password(argocd_2.secret.data["password"]))
